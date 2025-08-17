76c200aab0707d4fbbb1c9d176e52788f38fe770
devlopersumit <sumitsj2002@gmail.com>
2025-08-17 15:36:37 +0530

    fixed bugs

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -45,6 +45,7 @@ const Home = () => {
               </svg>
               Download Resume
             </button>
+            <a href="#contact">
             <button
               data-slot="button"
               className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg:not([className*='size-'])]:size-4 shrink-0 [&amp;_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-10 rounded-md px-6 has-[&gt;svg]:px-4 font-medium bg-transparent"
@@ -66,6 +67,7 @@ const Home = () => {
               </svg>
               Contact Me
             </button>
+            </a>
           </div>
           <div className="flex justify-center space-x-6">
             <a
